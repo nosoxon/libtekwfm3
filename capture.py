5931626cf479085611294b2d48f886ddba2d8ae7
Oliver Emery <code@nosoxon.net>
2024-06-02 19:28:02 -0700

    return to reasonability

--- capture.py
+++ capture.py
@@ -1,38 +1,7 @@
 from ctypes import *
 from .wfm import Wfm
 
-class PWrapper:
-    def __init__(self, ptr, length):
-        self.ptr = ptr
-        self.length = length
-
-    def __len__(self):
-        return self.length
-
-    def __getitem__(self, k):
-        if k < 0 or k >= self.length:
-            raise IndexError('list index out of range')
-        return self.ptr[k]
-
-class PPWrapper(PWrapper):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def __getitem__(self, k):
-        if k < 0 or k >= self.length:
-            raise IndexError('list index out of range')
-        return self.ptr[k].contents
-
-class ChildStrucutre(Structure):
-    @property
-    def parent(self):
-        return self._parent
-
-    @parent.setter
-    def parent(self, parent):
-        self._parent = parent
-
-class Channel(ChildStrucutre):
+class Channel(Structure):
     _fields_ = [
         ('min',                        c_double),
         ('max',                        c_double),
@@ -57,38 +26,10 @@ class Channel(ChildStrucutre):
         ('_falling_times',             POINTER(c_double)),
     ]
 
-    @property
-    def raw_data(self) -> list[int]:
-        return PWrapper(self._raw_data, self._parent.data_len)
-
-    @property
-    def data(self) -> list[float]:
-        return PWrapper(self._data, self._parent.data_len)
-
-    @property
-    def rising_edges(self) -> list[int]:
-        return PWrapper(self._rising_edges, self.nrising)
-
-    @property
-    def falling_edges(self) -> list[int]:
-        return PWrapper(self._falling_edges, self.nfalling)
-
-    @property
-    def rising_times(self) -> list[float]:
-        return PWrapper(self._rising_times, self.nrising)
-
-    @property
-    def falling_times(self) -> list[float]:
-        return PWrapper(self._falling_times, self.nfalling)
-
-class Frame(ChildStrucutre):
+class Frame(Structure):
     _fields_ = [
         ('_ch',                        POINTER(Channel)),
     ]
-
-    @property
-    def ch(self) -> list[Channel]:
-        return PWrapper(self._ch, self.parent.nch)
 
 class Capture(Structure):
     _fields_ = [
@@ -115,30 +56,7 @@ class Capture(Structure):
         ('_all_data',                  POINTER(c_double)),
     ]
 
-    @property
-    def v_scale(self) -> list[float]:
-        return PWrapper(self._v_scale, self.nch)
-
-    @property
-    def v_offset(self) -> list[float]:
-        return PWrapper(self._v_offset, self.nch)
-
-    @property
-    def times(self) -> list[float]:
-        return PWrapper(self._times, self.data_len)
-
-    @property
-    def frames(self) -> list[Frame]:
-        return PWrapper(self._frames, self.nframes)
-
-    @property
-    def wfms(self) -> list[Wfm]:
-        return PPWrapper(self._wfms, self.nch)
-
-    @property
-    def all_channels(self) -> list[Channel]:
-        return PWrapper(self._all_channels, self.nch * self.nframes)
-
-    @property
-    def all_data(self) -> list[float]:
-        return PWrapper(self._all_data, self.nch * self.nframes * self.record_len)
+    def wfm(self, n: int) -> Wfm:
+        if n > self.nch:
+            raise IndexError('list index out of range')
+        return self._wfms[n].contents
--- lib.py
+++ lib.py
@@ -36,11 +36,26 @@ def make_capture(wfm_paths: list[str], debounce: float) -> Pointer:
         raise Exception('failed to make_capture')
 
     capture = p_capture.contents
-    for f in range(capture.nframes):
-        capture.frames[f].parent = capture
-    for i in range(capture.nframes * capture.nch):
-        capture.all_channels[i].parent = capture
-    return p_capture
+    _pointers_to_lists(capture, {
+        'v_scale':         capture.nch,
+        'v_offset':        capture.nch,
+        'times':           capture.data_len,
+        'frames':          capture.nframes,
+        # 'all_channels':  capture.nch * capture.nframes,
+        # 'all_data':      capture.nch * capture.nframes * capture.record_len,
+    })
+    for frame in capture.frames:
+        _pointers_to_lists(frame, {'ch': capture.nch})
+        for ch in frame.ch:
+            _pointers_to_lists(ch, {
+                'raw_data':        capture.data_len,
+                'data':            capture.data_len,
+                'rising_edges':    ch.nrising,
+                'falling_edges':   ch.nfalling,
+                'rising_times':    ch.nrising,
+                'falling_times':   ch.nfalling,
+            })
+    return p_capture, capture
 
 def destroy_capture(p_capture: Pointer) -> int:
     if _lib.destroy_capture(p_capture) != 0:
@@ -49,3 +64,8 @@ def destroy_capture(p_capture: Pointer) -> int:
 def _get_cargs(paths: list[str]) -> tuple:
     return len(paths), (c_char_p * len(paths))(
             *(p.encode() for p in paths))
+
+def _pointers_to_lists(container, lengths: dict[str, int]):
+    for attr, length in lengths.items():
+        ptr = getattr(container, '_' + attr)
+        setattr(container, attr, [ptr[i] for i in range(length)])
